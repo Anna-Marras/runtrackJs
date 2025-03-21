a1c55d0e5ed85bfc211f15e8f3107af4c95dee35
Anna marras <anna-marras@laplateforme.io>
2025-03-21 12:23:31 +0100

    Mise à jour des fichiers

--- jour 09-10/bigjob/js/connexion.js
+++ jour 09-10/bigjob/js/connexion.js
@@ -11,23 +11,30 @@ e.preventDefault();
 
 const email = document.getElementById('email').value;
 const password = document.getElementById('password').value;
+const passwordConfirm = document.getElementById('password-confirm').value;
 
 if (!isValidDomain(email)){
-    message.textContent = "Seuls les membres de La Plateforme peuvent créer un compte.";
+    message.textContent = "Seuls les membres de La Plateforme peuvent se connecter.";
     message.style.color = "red";
     return;
 }
+
 if (password.length < 8){
     message.textContent = "Le mot de passe doit contenir au moins 8 caractères.";
     message.style.color = "red";
     return;
 }
 
-message.textContent = "Inscription réussie";
+if (password !== passwordConfirm){
+    message.textContent = "Vérifier votre mot passe.";
+    message.style.color = "red";
+    return;
+}
+
+message.textContent = "Connexion réussi.";
 message.style.color = "green";
 
 setTimeout(() => {
     window.location.href = "calendrier.html"; 
 }, 1000);
 });
-
